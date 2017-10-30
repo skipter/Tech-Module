c3138f052fcd97e29266be91b984312b1ec608cd
skipter@abv.bg <b0r1s-12345>
2017-10-30 09:34:39 +0200

    Add Regex Exercises

---
+++ Programming Fundamentals/Programming-Fundamentals-September-2017/Regular Expressions (RegEx) - Exercises/02.Extract Sentences by Keyword/ExtractSentencesByKeyword.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.Extract_Sentences_by_Keyword
+{
+    public class ExtractSentencesByKeyword
+    {
+        public static void Main()               // 100/100
+        {
+            string word = Console.ReadLine();
+            var input = Console.ReadLine().Split(new char[] { '.', '?', '!', }, StringSplitOptions.RemoveEmptyEntries);
+            string pattern = "\\b" + word + "\\b";
+            Regex regex = new Regex(pattern);
+            foreach (var sentance in input)
+            {
+                if (regex.IsMatch(sentance))
+                {
+                    Console.WriteLine(sentance.Trim());
+                }
+            }
+        }
+    }
+}
+                                // 80/100 ??
+//          using System;
+//          namespace _02.Extract_Sentences_by_Keyword
+//          {
+//              public class ExtractSentencesByKeyword
+//              {
+//                  public static void Main()
+//                  {
+//                      string word = Console.ReadLine();
+//                      var input = Console.ReadLine().Split(new char[] { '.', '?', '!', }, StringSplitOptions.RemoveEmptyEntries);
+//                      string extraWord = " " + word + " ";
+//                      foreach (var sentance in input)
+//                      {
+//                          if (sentance.Contains(extraWord))
+//                          {
+//                              Console.WriteLine(sentance.Trim('.', '!', '?', ' '));
+//                          }
+//                      }
+//                  }
+//              }
+//          }
---
+++ Programming Fundamentals/Programming-Fundamentals-September-2017/Regular Expressions (RegEx) - Exercises/04. Weather/Weather.cs
@@ -0,0 +1,43 @@
+﻿namespace _04.Weather
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using System.Linq;
+    public class Weather
+    {
+        public static void Main()
+        {
+            Dictionary<string, double> cityWithTemp = new Dictionary<string, double>();
+            Dictionary<string, string> cityWithWeather = new Dictionary<string, string>();
+            List<int> nlist = new List<int>();
+            string pattern = "([A-Z]{2})([0-9.]+)([a-zA-Z]+)\\|";
+            String input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            while (input != "end")
+            {
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Match match = Regex.Match(input, pattern);
+
+                    string city = match.Groups[1].Value;
+                    double temp = double.Parse(match.Groups[2].Value);
+                    string weather = match.Groups[3].Value;
+
+                    cityWithTemp[city] = temp;
+                    cityWithWeather[city] = weather;
+
+                }
+                input = Console.ReadLine();
+            }
+            Dictionary<string, double> sortedDictionryByTemp = cityWithTemp.OrderBy(c => c.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var cityy in sortedDictionryByTemp)
+            {
+                Console.WriteLine($"{cityy.Key} => {cityy.Value} => {cityWithWeather[cityy.Key]}");
+            }
+        }
+    }
+}
