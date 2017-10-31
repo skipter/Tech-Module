97d6f06260cea59b1cc98069d8f6c67aa28853a4
borismandjukov <borismandjukov@gmail.com>
2017-10-31 15:49:37 +0200

    Add some code to Exam Preparation III - Second exercise

--- Programming Fundamentals/Programming-Fundamentals-September-2017/Exam Preparation III/02.Command Intrepreter/CommandIntrepreter.cs
+++ Programming Fundamentals/Programming-Fundamentals-September-2017/Exam Preparation III/02.Command Intrepreter/CommandIntrepreter.cs
@@ -12,32 +12,67 @@
                 , StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
 
-            var commands = Console.ReadLine();
-            while (commands != "end")
+            string input = Console.ReadLine();
+            var reverse = new List<string>();
+            var sort = new List<string>();
+
+            while (input != "end")
             {
-                if (commands.Contains("reverse"))
+                var comm = input
+                    .Split(new char[] { ' ', }
+                    , StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                int start;
+                int end;
+
+                if (input.Contains("reverse"))
+                {
+                    start = int.Parse(comm[2]);
+                    end = int.Parse(comm[4]);
+                    if (start < end)
+                    {
+                        reverse = numbers.Skip(start).Reverse().Skip(end).ToList();
+                        numbers.RemoveRange(start, end);
+                        numbers.InsertRange(start, reverse);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input parameters.");
+                    }
+                }
+                else if (input.Contains("sort"))
+                {
+                    start = int.Parse(comm[2]);
+                    end = int.Parse(comm[4]);
+                   if (start < 0 || end < 0)
+                   {
+                        Console.WriteLine("Invalid input parameters.");
+                    }
+                    else
+                    {
+                        sort = numbers.Skip(start).Reverse().Skip(end).OrderBy(x => x).ToList();
+                        numbers.RemoveRange(start, end);
+                        numbers.InsertRange(start, sort);
+                    }
+
+                }
+                else if (input.Contains("rollLeft"))
+                {
+                    int index;
+                    var n = int.TryParse(comm[1], out index);
+                    index = index % numbers.Count;
+                    var remove = numbers.Take(index).ToList();
+                    numbers.RemoveRange(0, index);
+                    numbers.AddRange(remove);
+                }
+                else if (input.Contains("rollRight"))
                 {
 
                 }
-                if (commands.Contains("sort"))
-                {
-
-                }
-                if (commands.Contains("rollLeft"))
-                {
-
-                }
-                if (commands.Contains("rollRight"))
-                {
-
-                }
-
-
-
-
-
-                commands = Console.ReadLine();
+                input = Console.ReadLine();
             }
+            Console.WriteLine($"[{String.Join(", ", numbers)}]");
         }
     }
 }
