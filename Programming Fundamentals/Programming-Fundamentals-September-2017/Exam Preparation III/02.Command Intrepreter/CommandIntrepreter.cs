a7ab8b3d8fa6c2e6bb68f7a063e7648ea04a01e1
skipter@abv.bg <b0r1s-12345>
2017-10-31 09:19:52 +0200

    Add Exam preparation III - first two exercises

---
+++ Programming Fundamentals/Programming-Fundamentals-September-2017/Exam Preparation III/02.Command Intrepreter/CommandIntrepreter.cs
@@ -0,0 +1,43 @@
+﻿namespace _02.Command_Intrepreter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class CommandIntrepreter
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ', }
+                , StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var commands = Console.ReadLine();
+            while (commands != "end")
+            {
+                if (commands.Contains("reverse"))
+                {
+
+                }
+                if (commands.Contains("sort"))
+                {
+
+                }
+                if (commands.Contains("rollLeft"))
+                {
+
+                }
+                if (commands.Contains("rollRight"))
+                {
+
+                }
+
+
+
+
+
+                commands = Console.ReadLine();
+            }
+        }
+    }
+}
