c0a37040b325c9ebcc754b5b5fc1726470565c7a
borismandjukov <borismandjukov@gmail.com>
2017-11-02 18:30:06 +0200

    Try to resovle WinningTicket - not finished yet

--- Programming Fundamentals/Programming-Fundamentals-September-2017/Exam Preparation I/03. Endurance Rally/Endurance Rally.cs
+++ Programming Fundamentals/Programming-Fundamentals-September-2017/Exam Preparation I/03. Endurance Rally/Endurance Rally.cs
@@ -6,38 +6,55 @@
     public class Program
     {
         public static void Main()
-        {
+        {                       
+                                                    // 80/100 Point - Missing cases !!
             List<string> drivers = Console.ReadLine().Split(' ').ToList();
             double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
             int[] index = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             //List<int> index = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int reachedIndex = 0;
             for (int i = 0; i < drivers.Count; i++)
             {
                 double fuel = drivers[i][0];
-
+                int temp = 0;
                 for (int k = 0; k < numbers.Length; k++)
                 {
+                    temp++;
                     if (index.Contains(k))
                     {
                         fuel += numbers[k];
+                        if (fuel <= 0)
+                        {
+                            break;
+                        }
                     }
-                    if (numbers[k] > 0)
+                    else if (numbers[k] > 0)
                     {
                         fuel -= numbers[k];
+                        if (fuel <= 0)
+                        {
+                            //Console.WriteLine($"{drivers[i]} - reached 0");
+                            break;
+                        }
                     }
-                    if (numbers[k] < 0 && (!index.Contains(k)))
+                    else if (numbers[k] < 0 && (!index.Contains(k)))
                     {
                         fuel += Math.Abs(numbers[k]);
+                        if (fuel <= 0)
+                        {
+                            break;
+                        }
                     }
-                   
                 }
+                reachedIndex = temp;
+                temp = 0;
                 if (fuel >= 0)
                 {
                     Console.WriteLine($"{drivers[i]} - fuel left {fuel:F2}");
                 }
                 else
                 {
-                    Console.WriteLine($"{drivers[i]} - reached 0");
+                    Console.WriteLine($"{drivers[i]} - reached {reachedIndex - 1}");
                 }
             }
         }
---
+++ Programming Fundamentals/Programming-Fundamentals-September-2017/Exam Preparation I/04. Winning Ticket/WinningTicket.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.Winning_Ticket
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class WinningTicket
+    {
+        public static void Main()
+        {
+            var tickets = Console.ReadLine()
+                .Split(new char[] { ',', ' ', }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            string pattern = @"([$@#^]+)";
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Length == 20)
+                {
+                    string left = ticket.Substring(0, 10);
+                    string right = ticket.Substring(10, 10);
+                    int leftNum = 0;
+                    int rightNum = 0;
+                    int equalIndexSymbol = 0;
+                    Dictionary<string, int> ahh = new Dictionary<string, int>();
+                    ahh.Add("@", 0);
+                    ahh.Add("#", 0);
+                    ahh.Add("$", 0);
+                    ahh.Add("^", 0);
+
+                    
+            }
+        }
+    }
+}
