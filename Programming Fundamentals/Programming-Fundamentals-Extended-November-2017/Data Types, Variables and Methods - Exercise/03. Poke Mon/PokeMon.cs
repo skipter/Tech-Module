5344e63e8d3c614a16e0f2a150368a7d1f16951e
skipter@abv.bg <b0r1s-12345>
2017-11-11 20:24:05 +0200

    Add Extended November Folder and some exercises

---
+++ Programming Fundamentals/Programming-Fundamentals-Extended-November-2017/Data Types, Variables and Methods - Exercise/03. Poke Mon/PokeMon.cs
@@ -0,0 +1,15 @@
+﻿namespace _03.Poke_Mon
+{
+    using System;
+    public class PokeMon
+    {
+        public static void Main()
+        {
+            long pokePower = long.Parse(Console.ReadLine());
+            long distanceBetweenTargets = long.Parse(Console.ReadLine());
+            long exhaustionFactor = long.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
