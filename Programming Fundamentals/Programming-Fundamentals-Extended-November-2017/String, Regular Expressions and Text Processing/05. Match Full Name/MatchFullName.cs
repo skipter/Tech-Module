62c6ce4a7986bf20e035498a6672b5b5c5539aad
borismandjukov <borismandjukov@gmail.com>
2017-11-30 09:23:52 +0200

    Add String and Regex Lab Exercise

---
+++ Programming Fundamentals/Programming-Fundamentals-Extended-November-2017/String, Regular Expressions and Text Processing/05. Match Full Name/MatchFullName.cs
@@ -0,0 +1,20 @@
+﻿namespace _05.Match_Full_Name
+{
+    using System;
+    using System.Text.RegularExpressions;
+    public class MatchFullName
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            Regex regex = new Regex(pattern);
+
+            foreach (Match m in Regex.Matches(input, pattern))
+            {
+                Console.Write(m + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
