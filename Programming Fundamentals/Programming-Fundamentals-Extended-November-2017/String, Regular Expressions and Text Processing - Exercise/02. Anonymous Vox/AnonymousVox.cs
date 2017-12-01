674b71379028975f07f418da9cb20b8eecfcf2a0
borismandjukov <borismandjukov@gmail.com>
2017-12-01 21:28:10 +0200

    Add String and Regular Expressions Exercises

---
+++ Programming Fundamentals/Programming-Fundamentals-Extended-November-2017/String, Regular Expressions and Text Processing - Exercise/02. Anonymous Vox/AnonymousVox.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.Anonymous_Vox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    public class AnonymousVox
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            List<string> placeHolders = Console.ReadLine()
+                .Split(new char[] { '{', '}' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList(); //Split("{}".ToCharArray()) - в кавичките може всичко и го разбира на чарове
+
+            //Second way to Split the input .
+            //string[] placeHolders = Regex.Split(Console.ReadLine(), "[{}]")
+            //    .Where(x => x != String.Empty).ToArray();
+
+            string patternOne = @"([a-zA-Z]+)(.*)(\1)";
+
+            MatchCollection match = Regex.Matches(input, patternOne);
+            int counter = 0;
+            foreach (Match m in match)
+            {
+                string newString = m.Groups[1] + placeHolders[counter++] + m.Groups[3];
+                input = input.Replace(m.Value, newString);
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ Programming Fundamentals/Programming-Fundamentals-Extended-November-2017/String, Regular Expressions and Text Processing - Exercise/03. Hornet Comm/HornetComm.cs
@@ -0,0 +1,74 @@
+﻿namespace _03.Hornet_Comm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    public class HornetComm
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            List<string> message = new List<string>();
+            List<string> broadcast = new List<string>();
+            string privatePattern = @"^([0-9]+) <-> ([a-zA-Z0-9]+)$";
+            string broadcastPattern = @"^([^0-9]+) <-> ([a-zA-Z0-9]+)$";
+
+            while (input != "Hornet is Green")
+            {
+                var privatee = Regex.Match(input, privatePattern);
+                var broadcastMes = Regex.Match(input, broadcastPattern);
+
+                if (privatee.Success)
+                {
+                    string temp = privatee.Groups[1].ToString();
+                    temp = string.Join("", temp.ToCharArray().Reverse().ToArray());
+                    message.Add(temp + " -> " + privatee.Groups[2].ToString());
+                }
+
+                if (broadcastMes.Success)
+                {
+                    string temp = broadcastMes.Groups[2].ToString();
+                    string tempResult = String.Empty;
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        if (char.IsLower(temp[i]))
+                        {
+                            tempResult += temp[i].ToString().ToUpper();
+                        }
+                        else if (char.IsUpper(temp[i]))
+                        {
+                            tempResult += temp[i].ToString().ToLower();
+                        }
+                        else
+                        {
+                            tempResult += temp[i].ToString();
+                        }
+                    }
+                    broadcast.Add(tempResult + " -> " + broadcastMes.Groups[1]);
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Broadcasts:");
+            if (broadcast.Count == 0)
+            {
+                Console.WriteLine("None");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, broadcast));
+            }
+
+            Console.WriteLine("Messages:");
+            if (message.Count == 0)
+            {
+                Console.WriteLine("None");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, message));
+            }
+        }
+    }
+}
