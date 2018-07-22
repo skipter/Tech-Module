b9d3fa73413b6f11fbebab749cb21c5c068e053e
Boris Mandzhukov <skipter@abv.bg>
2018-07-22 17:21:09 +0300

    Php exercises, from 4 - 6.

---
+++ Software Technologies/PHP - Syntax and Basic Web/06. Sum Two Numbers.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sum Two Numbers</title>
+</head>
+<body>
+<?php
+    if (isset($_GET['num1']) && isset($_GET['num2'])) {
+        $num1 = intval($_GET['num1']);
+        $num2 = intval($_GET['num2']);
+        $sum = $num1 + $num2;
+        echo "$num1 + $num2 = $sum";
+    }
+?>
+<form>
+    <div>First Number</div>
+    <input type = "number" name = "num1"/>
+    <div>Second Number</div>
+    <input type = "number" name = "num2"/>
+    <div><input type = "submit"/></div>
+</form>
+</body>
+</html>
