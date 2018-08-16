e52ea72f06fa570f221362e3abbe3e0d609bffc1
Boris Mandzhukov <skipter@abv.bg>
2018-08-16 15:33:37 +0300

    Start exercise 03. Have to do the logic part.

--- Software Technologies/Java Basic Syntax/src/ThreeIntegersSum.java
+++ Software Technologies/Java Basic Syntax/src/ThreeIntegersSum.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class ThreeIntegersSum {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+
+        String[] split = (input.split(" "));
+
+        int[] numbers = new int[split.length];
+
+        for (int i = 0; i < split.length; i++) {
+            numbers[i] = Integer.parseInt(split[i]);
+        }
+
+        //ToDo logic operations.
 
     }
 }
