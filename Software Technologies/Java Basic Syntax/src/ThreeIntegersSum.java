d4200676ab68d2ceadd9ddea71efa4deaa5b5ed7
Boris Mandzhukov <skipter@abv.bg>
2018-08-16 15:44:38 +0300

    Logic ready.

--- Software Technologies/Java Basic Syntax/src/ThreeIntegersSum.java
+++ Software Technologies/Java Basic Syntax/src/ThreeIntegersSum.java
@@ -4,9 +4,7 @@ public class ThreeIntegersSum {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         String input = scanner.nextLine();
-
         String[] split = (input.split(" "));
 
         int[] numbers = new int[split.length];
@@ -15,7 +13,20 @@ public class ThreeIntegersSum {
             numbers[i] = Integer.parseInt(split[i]);
         }
 
-        //ToDo logic operations.
+        int firstNum = numbers[0];
+        int secondNum = numbers[1];
+        int thirdNum = numbers[2];
 
+        //ToDo Print the elements, in such way, that num1 <= num2.
+
+        if (firstNum + secondNum == thirdNum) {
+            System.out.println(firstNum + " + " +secondNum + " = " + thirdNum);
+        } else if (secondNum + thirdNum == firstNum) {
+            System.out.println(secondNum + " + " +thirdNum + " = " + firstNum);
+        } else if (firstNum + thirdNum == secondNum) {
+            System.out.println(firstNum + " + " +thirdNum + " = " + secondNum);
+        } else {
+            System.out.println("No");
+        }
     }
 }
