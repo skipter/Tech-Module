6fb80da4e651f07f9daacb87a452db9bbe246a1a
Boris Mandzhukov <skipter@abv.bg>
2018-08-20 15:59:09 +0300

    Upload 07 and 08 from Java Exercise.

--- Software Technologies/Java Basic Syntax Exercises/src/MaxSequenceOfIncreasingElements.java
+++ Software Technologies/Java Basic Syntax Exercises/src/MaxSequenceOfIncreasingElements.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MaxSequenceOfEqualElements {
+public class MaxSequenceOfIncreasingElements {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +17,7 @@ public class MaxSequenceOfEqualElements {
         int length = 0;
 
         for (int i = 0; i < numbers.length - 1; i++) {
-            if (numbers[i] == numbers[i + 1]) {
+            if (numbers[i] < numbers[i + 1]) {
                 length++;
                 if (i + 1 == numbers.length - 1 && maxLength < length) {
                     maxLength = length;
@@ -35,8 +35,10 @@ public class MaxSequenceOfEqualElements {
             }
         }
 
+        endIndex = endIndex - maxLength;
         for (int i = 0; i < maxLength + 1; i++) {
             System.out.print(numbers[endIndex] + " ");
+            endIndex++;
         }
     }
 }
---
+++ Software Technologies/Java Basic Syntax Exercises/src/MostFrequentNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MostFrequentNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s");
+        int[] numbers = new int[input.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
+        }
+
+        if (true) {
+            
+        }
+
+
+
+    }
+}
