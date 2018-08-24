904b84344614f298c1495a0c41da65a194dd4838
Boris Mandzhukov <skipter@abv.bg>
2018-08-24 06:05:36 +0300

    Add Java Web Calculator exercise.

---
+++ Software Technologies/Java Basic Web Calculator/src/main/java/com/softuni/entity/Calculator.java
@@ -0,0 +1,31 @@
+package com.softuni.entity;
+
+public class Calculator {
+    private double leftOperand;
+    private double rightOperand;
+    private String operator;
+
+    public double getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(double leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public double getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(double rightOperand) {
+        this.rightOperand = rightOperand;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
