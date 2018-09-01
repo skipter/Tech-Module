deaaa5ff95e5630e9e809723fb0167caf1cc14af
Boris Mandzhukov <skipter@abv.bg>
2018-09-01 13:22:32 +0300

    Add Article controller and create form.

---
+++ Software Technologies/Java Blog Basic Functionality/blog/src/main/java/softuniBlog/controller/ArticleController.java
@@ -0,0 +1,26 @@
+package softuniBlog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import softuniBlog.repository.ArticleRepository;
+import softuniBlog.repository.UserRepository;
+
+@Controller
+public class ArticleController {
+
+    @GetMapping("/article/create")
+    @PreAuthorize("isAuthenticated()")
+    public String create(Model model) {
+        model.addAttribute("view", "article/create");
+        return "base-layout";
+    }
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+}
